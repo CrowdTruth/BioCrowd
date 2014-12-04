a5903446f700248b28dfdd5e992cb7df0ba48733
MerelvanEmpel <MerelvanEmpel@users.noreply.github.com>
2014-12-04 10:17:26 +0100

    Changed the header of the game to the correct name

--- app/lang/en/gamelabels.php
+++ app/lang/en/gamelabels.php
@@ -2,7 +2,7 @@
 
 return array(
     'title' => '..:: Dr. Detective ::..',
-	'header.title' => 'Welcome to Dr. Watson,',
+	'header.title' => 'Welcome to Dr. Detective,',
 	'header.subtitle' => 'the place to help yourself by helping others',
 	'header.right' => '12.000.342 annotations in total<br />&nbsp;&nbsp;&nbsp;&nbsp;5.234 annotations this week',
 );
