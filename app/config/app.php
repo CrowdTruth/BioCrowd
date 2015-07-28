ec1521839bb8320d3685eabc8415306516ab5aeb
MerelvanEmpel <merelvanempel@gmail.com>
2015-07-28 16:52:41 +0200

    test

--- app/config/app.php
+++ app/config/app.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'url' => 'http://localhost',
+	'url' => 'http://game.crowdtruth.org/biocrowd',
 
 	/*
 	|--------------------------------------------------------------------------
