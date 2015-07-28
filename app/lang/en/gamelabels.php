4098b5f46ab6185df56b66654e2a74f378763d2f
MerelvanEmpel <merelvanempel@gmail.com>
2015-07-28 12:34:27 +0200

    Made logo text bold

--- app/lang/en/gamelabels.php
+++ app/lang/en/gamelabels.php
@@ -2,7 +2,7 @@
 
 return array(
 	'gamename' => 'BioCrowd',
-	'logoText' => '<div id="logoColor1" style="display: inline;">Bio</div><div id="logoColor2" style="display: inline;">Crowd</div>',
+	'logoText' => '<b><div id="logoColor1" style="display: inline;">Bio</div><div id="logoColor2" style="display: inline;">Crowd</div></b>',
 	'gameOverviewText' => 'In biological and medical research, <b>counting cells, nuclei or colonies</b> is a critical step in order to determine the growth rate of the cells. 
 		The growth rate of cells is a relevant factor in experiments that are done in the field of <b>cancer research</b>. 
 		Another relevant factor for these experiments is the <b>location of vesicles</b>, which can indicate transport of debree from the cancer inducing factor, 
