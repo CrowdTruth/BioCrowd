850af4589d11588017381563f0436fabdcc19e89
MerelvanEmpel <merelvanempel@gmail.com>
2015-09-17 11:23:07 +0200

    Fixed the spelling of debris...

--- app/lang/en/gamelabels.php
+++ app/lang/en/gamelabels.php
@@ -6,7 +6,7 @@ return array(
 	'logoText' => '<b><div id="logoColor1" style="display: inline;">Bio</div><div id="logoColor2" style="display: inline;">Crowd</div></b>',
 	'gameOverviewText' => 'In biological and medical research, <b>counting cells, nuclei or colonies</b> is a critical step in order to determine the growth rate of the cells. 
 		The growth rate of cells is a relevant factor in experiments that are done in the field of <b>cancer research</b>. 
-		Another relevant factor for these experiments is the <b>location of vesicles</b>, which can indicate transport of debree from the cancer inducing factor, 
+		Another relevant factor for these experiments is the <b>location of vesicles</b>, which can indicate transport of debris from the cancer inducing factor, 
 		after successful treatment. <br>
 		We would like to ask you to help us with these tasks. In return, you will receive <b>points for your score</b>, so you can <b>level up, 
 		gain badges</b> and <b>practice your cell biology</b> skills at the same time!',
