7db071ba38db70364d3780b5fd41d1be82ff7c46
MerelvanEmpel <merelvanempel@gmail.com>
2015-10-07 15:06:45 +0200

    Fixed BUG: Not able to start playing without password

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -57,13 +57,13 @@ class LoginController extends BaseController {
 			return Redirect::to('login')->with('flash_error', 'e-mail adress does not exist. ')->withInput();
 		}
 		$nameLength = strlen($name);
-		$passLength = strlen($pass);
+		//$passLength = strlen($pass); Turned this off because of automatic login without password
 		if(!($nameLength >= 2)) {
 			return Redirect::to('login')->with('flash_error', 'Your name must be at least 2 characters long')->withInput();
 		}
-		if(!($passLength >= 4)) {
+		/*if(!($passLength >= 4)) {
 			return Redirect::to('login')->with('flash_error', 'Your password must be at least 4 characters long')->withInput();
-		}
+		}*/
 		try {
 			//to do: Validate the user exists with an e-mail activation of the account
 			//Check if the email already exists
