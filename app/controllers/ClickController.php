c4ac14d878772f22f8acbeadde1a00bb97f39ecf
MerelvanEmpel <merelvanempel@gmail.com>
2016-08-18 09:26:45 +0200

    Fix error message when user is logged out before the last click can be saved in the ClickController

--- app/controllers/ClickController.php
+++ app/controllers/ClickController.php
@@ -5,12 +5,14 @@
 */
 class ClickController extends BaseController {
 	public function submitUserAction(){
-		$page = Input::get('page');
-		$id = Input::get('id');
-		$userAction = New UserAction();
-		$userAction->user_id = Auth::user()->get()->id;
-		$userAction->page = $page;
-		$userAction->action = $id;
-		$userAction->save();
+		if(Auth::user()->check()){
+			$page = Input::get('page');
+			$id = Input::get('id');
+			$userAction = New UserAction();
+			$userAction->user_id = Auth::user()->get()->id;
+			$userAction->page = $page;
+			$userAction->action = $id;
+			$userAction->save();
+		}
 	}
 }
