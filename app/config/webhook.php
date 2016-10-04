2cddf92eedff2c3976a508db33d25167bee6eb3b
MerelvanEmpel <merelvanempel@gmail.com>
2016-10-04 14:56:59 +0200

    Fix the API URL

--- app/config/webhook.php
+++ app/config/webhook.php
@@ -1,6 +1,6 @@
 <?php
 
 return array(
-    'URL' => 'game.crowdtruth.org/biocrowd',
+    'URL' => 'http://dev.crowdtruth.org/game/detective/',
 	'chunksize' => '20', //This is the maximal amount of judgements sent to Crowdtruth via the API per chunk
 );
