b9da561174827d253475024661021ff2558eccd0
MerelvanEmpel <merelvanempel@gmail.com>
2016-10-04 16:53:56 +0200

    Set chunksize to 1 default

--- app/config/webhook.php
+++ app/config/webhook.php
@@ -2,5 +2,7 @@
 
 return array(
     'URL' => 'http://dev.crowdtruth.org/game/detective/',
-	'chunksize' => '20', //This is the maximal amount of judgements sent to Crowdtruth via the API per chunk
+	//This is the maximal amount of judgements sent to Crowdtruth via the API per chunk. 
+	//Set this higher for better performance, but the trade off is bigger post data (which could result in vague errors) 
+	'chunksize' => '1', 
 );
